da2f62d0c9434cf72a6058989abb6876b6d626c2
Ernesto Laval <ernesto.laval@gmail.com>
2016-04-03 23:58:36 +0100

    Digital outputs

--- MyBLE/MyBLE.ino
+++ MyBLE/MyBLE.ino
@@ -17,6 +17,8 @@
 */
 #include <CurieBle.h>
 
+const int ledPin = 7; // set ledPin to use on-board LED
+
 /*
    This sketch example partially implements the standard Bluetooth Low-Energy Battery service.
    For more information: https://developer.bluetooth.org/gatt/services/Pages/ServicesHome.aspx
@@ -36,6 +38,23 @@ BLECharacteristic pin5Char("2A1F", BLERead | BLENotify,2);     // remote clients
 
 BLECharacteristic mychars[6] = {pin0Char, pin1Char, pin2Char, pin3Char, pin4Char, pin5Char };
 
+// create switch characteristic and allow remote device to read and write
+BLECharCharacteristic pinD0Char("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+BLECharCharacteristic pinD1Char("19B10001-E8F2-537E-4F6C-D104768A1215", BLERead | BLEWrite);
+BLECharCharacteristic pinD2Char("19B10001-E8F2-537E-4F6C-D104768A1216", BLERead | BLEWrite);
+BLECharCharacteristic pinD3Char("19B10001-E8F2-537E-4F6C-D104768A1217", BLERead | BLEWrite);
+BLECharCharacteristic pinD4Char("19B10001-E8F2-537E-4F6C-D104768A1218", BLERead | BLEWrite);
+BLECharCharacteristic pinD5Char("19B10001-E8F2-537E-4F6C-D104768A1219", BLERead | BLEWrite);
+BLECharCharacteristic pinD6Char("19B10001-E8F2-537E-4F6C-D104768A1220", BLERead | BLEWrite);
+BLECharCharacteristic pinD7Char("19B10001-E8F2-537E-4F6C-D104768A1221", BLERead | BLEWrite);
+BLECharCharacteristic pinD8Char("19B10001-E8F2-537E-4F6C-D104768A1222", BLERead | BLEWrite);
+BLECharCharacteristic pinD9Char("19B10001-E8F2-537E-4F6C-D104768A1223", BLERead | BLEWrite);
+BLECharCharacteristic pinD10Char("19B10001-E8F2-537E-4F6C-D104768A1224", BLERead | BLEWrite);
+BLECharCharacteristic pinD11Char("19B10001-E8F2-537E-4F6C-D104768A1225", BLERead | BLEWrite);
+BLECharCharacteristic pinD12Char("19B10001-E8F2-537E-4F6C-D104768A1226", BLERead | BLEWrite);
+BLECharCharacteristic pinD13Char("19B10001-E8F2-537E-4F6C-D104768A1227", BLERead | BLEWrite);
+
+BLECharCharacteristic myDchars[14] = {pinD0Char,pinD1Char,pinD2Char,pinD3Char,pinD4Char,pinD5Char,pinD6Char,pinD7Char,pinD8Char,pinD9Char,pinD10Char,pinD11Char,pinD12Char,pinD13Char };
 
     
 // get notifications if this characteristic changes
@@ -90,12 +109,45 @@ void setup() {
   blePeripheral.setAdvertisedServiceUuid(batteryService.uuid());  // add the service UUID
   blePeripheral.addAttribute(batteryService);   // Add the BLE Battery service
 
+  //blePeripheral.addAttribute(switchChar);
+
+  // Configure analog ports
   for (int i=0; i <= 5; i++){
     blePeripheral.addAttribute(mychars[i]);
 
     const unsigned char pinCharArray[2] = { (char)0, (char)0 };
     mychars[i].setValue(pinCharArray, 2);
   } 
+
+
+  // Configure digital ports
+  for (int i=4; i <= 13; i++) {
+    
+    blePeripheral.addAttribute(myDchars[i]);
+    myDchars[i].setValue(0);
+    //myDchars[i].setEventHandler(BLEWritten, switchCharacteristicWritten);;
+  } 
+
+  myDchars[4].setEventHandler(BLEWritten, listener4);
+  myDchars[5].setEventHandler(BLEWritten, listener5);
+  myDchars[6].setEventHandler(BLEWritten, listener6);
+  myDchars[7].setEventHandler(BLEWritten, listener7);
+  myDchars[8].setEventHandler(BLEWritten, listener8);
+  myDchars[9].setEventHandler(BLEWritten, listener9);
+  myDchars[10].setEventHandler(BLEWritten, listener10);
+  myDchars[11].setEventHandler(BLEWritten, listener11);
+  myDchars[12].setEventHandler(BLEWritten, listener12);
+  myDchars[13].setEventHandler(BLEWritten, listener13);
+
+  
+
+    //blePeripheral.addAttribute(myDchars[7]);
+    //myDchars[7].setValue(0);
+    //myDchars[7].setEventHandler(BLEWritten, switchCharacteristicWritten);
+  // assign event handlers for characteristic
+  //switchChar.setEventHandler(BLEWritten, switchCharacteristicWritten);
+  // set an initial value for the characteristic
+  //switchChar.setValue(0);
 
   /* Now activate the BLE device.  It will start continuously transmitting BLE
      advertising packets and will be visible to remote BLE central devices
@@ -147,7 +199,7 @@ void sendAnalogValue(int pinNum) {
 
   int val2 = pinValue/256;
   int val1 = pinValue - val2*256;
-
+    /*
     Serial.print("Sending value for analog pin "); // print it
     Serial.print(pinNum); // print it      
     Serial.print(" : "); // print it
@@ -156,6 +208,7 @@ void sendAnalogValue(int pinNum) {
     Serial.print(val1);
     Serial.print(" - "); // print it
     Serial.println(pinValue);
+    */
 
     const unsigned char pinCharArray[2] = { (char)val2, (char)val1 };
     mychars[pinNum].setValue(pinCharArray, 2);  // and update the heart rate measurement characteristic
@@ -175,3 +228,60 @@ bool haschangedAnalog(int pinNum) {
 
 }
 
+
+void listener4(BLECentral& central, BLECharacteristic& characteristic) {
+  switchCharacteristicWritten(central, characteristic,  4);
+}
+
+void listener5(BLECentral& central, BLECharacteristic& characteristic) {
+  switchCharacteristicWritten(central, characteristic,  5);
+}
+
+void listener6(BLECentral& central, BLECharacteristic& characteristic) {
+  switchCharacteristicWritten(central, characteristic,  6);
+}
+
+void listener7(BLECentral& central, BLECharacteristic& characteristic) {
+  switchCharacteristicWritten(central, characteristic,  7);
+}
+
+void listener8(BLECentral& central, BLECharacteristic& characteristic) {
+  switchCharacteristicWritten(central, characteristic,  8);
+}
+
+void listener9(BLECentral& central, BLECharacteristic& characteristic) {
+  switchCharacteristicWritten(central, characteristic,  9);
+}
+
+void listener10(BLECentral& central, BLECharacteristic& characteristic) {
+  switchCharacteristicWritten(central, characteristic,  10);
+}
+
+void listener11(BLECentral& central, BLECharacteristic& characteristic) {
+  switchCharacteristicWritten(central, characteristic,  11);
+}
+
+void listener12(BLECentral& central, BLECharacteristic& characteristic) {
+  switchCharacteristicWritten(central, characteristic,  12);
+}
+void listener13(BLECentral& central, BLECharacteristic& characteristic) {
+  switchCharacteristicWritten(central, characteristic,  13);
+}
+
+void switchCharacteristicWritten(BLECentral& central, BLECharacteristic& characteristic, int pinNum) {
+  // central wrote new value to characteristic, update LED
+  Serial.print("Characteristic event, written: ");
+  Serial.print(pinNum);  
+  Serial.print(" - ");  
+  Serial.print(characteristic.properties());  
+  Serial.print(" - ");
+  Serial.print(*characteristic.value());
+
+  if (*characteristic.value() == 1) {
+    Serial.println("LED on");
+    digitalWrite(myDPins[pinNum], HIGH);
+  } else {
+    Serial.println("LED off");
+    digitalWrite(myDPins[pinNum], LOW);
+  }
+}
